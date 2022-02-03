151674b7c084c5d0593beb172175d8cbd389cadc
Stefan Frank <62906492+Stefansfrank@users.noreply.github.com>
2022-02-03 08:31:59 -0500

    Faster version of Day 14

--- Day14.kt
+++ Day14.kt
@@ -1,24 +1,46 @@
 package com.sf.aoc2016
 
-import java.math.BigInteger
 import java.security.MessageDigest
+import kotlin.experimental.and
 
 class Day14 : Solver {
 
     // using the Java library for MD5
-    private fun md5(inp:String):String =
-        BigInteger(1, MessageDigest.getInstance("MD5").digest(inp.toByteArray()))
-            .toString(16).padStart(32, '0')
+    private fun md5(inp:String):ByteArray = MessageDigest.getInstance("MD5").digest(inp.toByteArray())
 
-    // one hey with the index of the 3 seq and the index of the corroborating 5 index
-    data class Key(val c: Char, val ix3: Long, var ix5: Long)
+    // checks whether three subsequent half bytes are the same and return its value as Int
+    // the code is slightly ugly but fast
+    private fun check3(b1:Byte, b2:Byte):Int {
+        val d1 = b1.and(-16).rotateRight(4); val d2 = b1.and(15)
+        val d3 = b2.and(-16).rotateRight(4); val d4 = b2.and(15)
+        if (d2 == d3 && (d1 == d2 || d3 == d4)) return d2.toInt()
+        return -1
+    }
+
+    // checks whether three subsequent half bytes are the same and return its value as Int
+    // the code is slightly ugly but fast
+    private fun check5(b1:Byte, b2:Byte, b3:Byte):Int {
+        val d1 = b1.and(-16).rotateRight(4); val d2 = b1.and(15)
+        val d3 = b2.and(-16).rotateRight(4); val d4 = b2.and(15)
+        val d5 = b3.and(-16).rotateRight(4); val d6 = b3.and(15)
+        if (d2 == d3 && d3 == d4 && d4 == d5 && (d1 == d2 || d5 == d6)) return d2.toInt()
+        return -1
+    }
+
+    // converting a byte into a string with two hex letters - trying to be fast
+    private val bmp = listOf('0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f')
+    private fun b2s(b:Byte):String {
+        val d1 = b.and(-16).rotateRight(4); val d2 = b.and(15)
+        return "${bmp[d1.toInt()]}${bmp[d2.toInt()]}"
+    }
+
+    // one key with the index of the 3 seq and the index of the corroborating 5 seq
+    data class Key(val c: Int, val ix3: Long, var ix5: Long)
 
     // the actual find function with a cutoff amount of values (which I keep higher than 64
     // since I identify keys in sequence of their corroborating test, so the 64th key
-    // could be corroborated after the 65th key ...
+    // could be corroborated after the 65th key ...)
     private fun find(cutoff: Int, salt: String, rpt: Int):List<Key> {
-        val re3 = "([0-9a-f])\\1\\1".toRegex()
-        val re5 = "([0-9a-f])\\1\\1\\1\\1".toRegex()
 
         var cnt  = 0L
 
@@ -26,29 +48,42 @@ class Day14 : Solver {
         val open = List(16){ mutableListOf<Key>() }
         val wins = mutableListOf<Key>()
         while (true) {
-            var hash = "$salt$cnt"
-            repeat(rpt) { hash = md5(hash) }
 
-            // try to match 5 using Regex
-            var m = re5.find(hash)
-            if (m != null) {
-                val c = m.groupValues[1][0]
-
-                // go through all open keys for this letter
-                for (k in open[if (c.isDigit()) c - '0' else c - 'W']) {
-                    if (k.ix5 == -1L && (cnt - k.ix3) <= 1000) {
-                        k.ix5 = cnt
-                        wins.add(k)
-                        if (wins.size >= cutoff) return wins
-                    }
-                }
+            // for speed reasons I keep both a byte array of the new hash and a string variant around
+            var sHash = "$salt$cnt"
+            var bHash = byteArrayOf()
+            repeat(rpt) {
+                bHash = md5(sHash)
+                sHash = bHash.fold(""){ac, b -> ac + b2s(b) }
             }
 
-            // try to match 3
-            m = re3.find(hash)
-            if (m != null) {
-                val c = m.groupValues[1][0]
-                open[if (c.isDigit()) c - '0' else c - 'W'].add(Key(c, cnt, -1L))
+            // try to find the sequences using byte manipulations
+            // I do both 3/5 detection in one loop
+            var found5 = false
+            var found3 = false
+            for (ix in 0 .. 13) { // MD5 byte hashes are always 16 bytes long
+                if (!found5) {
+                    val t5 = check5(bHash[ix], bHash[ix+1], bHash[ix+2])
+                    if (t5 > -1) {
+                        for (k in open[t5]) {
+                            if (k.ix5 == -1L && (cnt - k.ix3) <= 1000) {
+                                k.ix5 = cnt
+                                wins.add(k)
+                                if (wins.size >= cutoff) return wins
+                            }
+                        }
+                        found5 = true
+                    }
+                }
+                if (!found3) {
+                    val t3 = check3(bHash[ix], bHash[ix+1])
+                    if (t3 > -1) { open[t3].add(Key(t3, cnt, -1L)); found3 = true }
+                }
+            }
+            // since I stopped in the loop above at 13, I need to test one more byte pair
+            if (!found3) {
+                val t3 = check3(bHash[14], bHash[15])
+                if (t3 > -1) open[t3].add(Key(t3, cnt, -1L))
             }
             cnt++
         }
